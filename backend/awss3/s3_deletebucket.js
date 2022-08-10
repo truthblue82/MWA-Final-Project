7b51beaabe26df6c11dfc26a4aa0c09d7b3ea185
Yen Truong <truthblue82@gmail.com>
2022-08-09 20:01:32 -0500

    just test s3

---
+++ backend/awss3/s3_deletebucket.js
@@ -0,0 +1,21 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Create params for S3.deleteBucket
+var bucketParams = {
+  Bucket : 'BUCKET_NAME'
+};
+
+// Call S3 to delete the bucket
+s3.deleteBucket(bucketParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
---
+++ backend/awss3/s3_listobject.js
@@ -0,0 +1,21 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Create the parameters for calling listObjects
+var bucketParams = {
+  Bucket : 'BUCKET_NAME',
+};
+
+// Call S3 to obtain a list of the objects in the bucket
+s3.listObjects(bucketParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
