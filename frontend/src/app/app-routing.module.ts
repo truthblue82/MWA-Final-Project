cae28e8a3c894e96518fb2b7edfdbde593518cce
Yen Truong <truthblue82@gmail.com>
2023-10-10 22:02:08 -0500

    remove preloader

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -30,9 +30,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {
-      preloadingStrategy: PreloadAllModules
-    })
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule],
   providers: []
